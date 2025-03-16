f16d3728169a2ae0882c3fde161b6c6acb51d47b
iavci <iavci@LAPTOP-4674SJUE>
2025-03-16 14:34:23 +0300

    Add project files.

---
+++ Ilayda_namespace/Hesaplama.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ilayda_namespace
+{
+    public class Hesaplama
+    {
+        public int sayiToplam(int sayi1, int sayi2)
+        {
+            return sayi1 + sayi2;
+        }
+        public int sayiCarp(int sayi1, int sayi2)
+        {
+            return sayi1 * sayi2;
+        }
+
+    }
+}
---
+++ Ilayda_namespace/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using Ilayda_namespace;
+
+namespace TestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hesaplama hesap = new Hesaplama();
+            int toplam = hesap.sayiToplam(16, 20);
+            int carpim = hesap.sayiCarp(29, 20);
+
+            Console.WriteLine("Toplam: " + toplam);
+            Console.WriteLine("Çarpım: " + carpim);
+        }
+    }
+}
